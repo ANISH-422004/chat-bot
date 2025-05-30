193fa8434770d209e6b7b21508eaa11847f89713
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-05-30 12:48:05 +0530

    build file made

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // keep '/' for root domain
   plugins: [react(),
   tailwindcss(),
   ],
 })
+
